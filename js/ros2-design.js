7d6d8a72f9b17a8e4b35f94a5529802157b65d42
William Woodall <william@osrfoundation.org>
2013-11-22 18:06:35 -0800

    improve table look and feel

--- js/ros2-design.js
+++ js/ros2-design.js
@@ -1,3 +1,6 @@
 $( document ).ready(function() {
+    // Add table of contents title to toc generated from markdown pages
     $( 'div.maruku_toc' ).prepend('<p><strong>Table of Contents</strong></p>');
+    // Add the bootstrap table class to table's inside of div's with table class
+    $( 'div.table table' ).addClass('table').addClass('table-striped');
 });
