5902ae1cd3a127ce98a9d6a637023f99db91f729
Tully Foote <tfoote@osrfoundation.org>
2016-02-03 01:56:34 -0800

    switch over to use rouge and GFM

--- test_local_deploy.sh
+++ test_local_deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-docker build -t "ros_tutorials" .
-
-docker run -v `pwd`:/tmp/jekyll -w /tmp/jekyll -i -t -p 0.0.0.0:4000:4000 ros_tutorials
+docker run --rm -v `pwd`:/srv/jekyll -i -t -p 127.0.0.1:4000:4000 jekyll/jekyll:pages
